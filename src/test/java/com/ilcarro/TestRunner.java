13df0af606e9875a38c2701d97a2e32ede0d72ba
zavvod <zavvod@gmail.com>
2026-02-12 19:28:50 +0100

    upd4

--- src/test/java/com/ilcarro/TestRunner.java
+++ src/test/java/com/ilcarro/TestRunner.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
         glue = "com/ilcarro/stepDefinitions",
         tags = "@wrongPassword",
         plugin = {"pretty","json:build/cucumber-report/cucumber.json"}
-
 )
 public class TestRunner {
 }
